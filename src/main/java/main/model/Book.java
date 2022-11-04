858dedaa53bb4872ea17234bb7b456abd8aa9bb6
vitaly beloserkovskii <vitaly.belo@gmail.com>
2022-11-04 15:07:53 +0300

    init

--- src/main/java/main/model/Book.java
+++ src/main/java/main/model/Book.java
@@ -1,11 +1,17 @@
-package response;
+package main.model;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
     private String name;
     private int year;
 
