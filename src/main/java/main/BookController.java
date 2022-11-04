330ad7f8c923dd90b2f0d1853a3531efb09c6e46
vitaly beloserkovskii <vitaly.belo@gmail.com>
2022-11-04 16:04:45 +0300

    init

--- src/main/java/main/BookController.java
+++ src/main/java/main/BookController.java
@@ -1,22 +1,46 @@
 package main;
 
+import main.model.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 import main.model.Book;
 
 @RestController
 public class BookController {
 
+    @Autowired
+    private BookRepository bookRepository;
+
     @GetMapping("/books/")
     public List<Book> list() {
-        return Storage.getAllBooks();
+        Iterable<Book> bookIter = bookRepository.findAll();
+        List<Book> books = new ArrayList<>();
+        for (Book book : bookIter) {
+            books.add(book);
+        }
+        return books;
+    }
+    @GetMapping("/books/{id}")
+    public ResponseEntity get(@PathVariable int id) {
+
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if (optionalBook.isPresent()) {
+            return new ResponseEntity(optionalBook.get(), HttpStatus.OK);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Data record not found");
     }
 
     @PostMapping("/books/")
     public int add(Book book) {
-        return Storage.addBook(book);
+        Book b = bookRepository.save(book);
+        return b.getId();
     }
 
     @DeleteMapping("/books/{id}")
@@ -34,13 +58,4 @@ public class BookController {
         return Storage.editBook(book);
     }
 
-    @GetMapping("/books/{id}")
-    public ResponseEntity get(@PathVariable int id) {
-
-        Book book = Storage.getBook(id);
-        if (book == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("null");
-        }
-        return new ResponseEntity(book, HttpStatus.OK);
-    }
 }
